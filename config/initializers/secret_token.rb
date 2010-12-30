cfb90b03e1c88530e6ccd984d0e9bfd1c1623e68
iluxan <iluxan@gmail.com>
2010-12-30 00:16:29 -0500

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outboard::Application.config.secret_token = 'cc24a8d623e61016b53195e47528b533751718ddc3d34448d2c78189d15cf4a00390a0415e8c95c5d3d4a3ac31a8bf73bdaf83aea19153759c2401fc8e142a95'
