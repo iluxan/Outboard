89fc7cc4343ee61fba60355861a417255b87b7cd
iluxan <iluxan@gmail.com>
2011-02-13 01:18:08 -0500

    create notes from doc, with paragraph included

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,9 +3,12 @@ class Note
   include Mongoid::Timestamps
   field :name
   field :content
+  field :paragraph
+  field :parahash
   field :tags
   field :created_at, :type => Time
   field :updated_at, :type => Time
-  validates_presence_of :name
+  validates_presence_of :content
   referenced_in :document
+  validates_presence_of :document_id
 end
